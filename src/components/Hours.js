9e51c7578126e53204936715ade7f8ea1d279d53
Damian Duque <99921190+elduke28@users.noreply.github.com>
2024-08-28 18:13:24 +0000

    Add componente de hora

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,25 @@
 // Placeholder to host a component
+// components/Hours.js
+import React from 'react';
+
+const Hours = () => {
+  const currentDate = new Date();
+  const currentDay = currentDate.getDay();
+  const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+  const currentDayName = daysOfWeek[currentDay];
+
+  const hours = Array.from({ length: 24 }, (_, i) => i);
+
+  return (
+    <div>
+      <h2>Hours for {currentDayName}</h2>
+      <ul>
+        {hours.map(hour => (
+          <li key={hour}>{hour}:00</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Hours;
